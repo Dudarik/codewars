6c3a1272e28c266e1331d7692ccc37aaaf3e0846
Dudarik <Dudarik@yandex.ru>
2020-01-27 10:19:52 +0500

    решил задачу удаления лишних элементов из массива

--- js/1.js
+++ js/1.js
@@ -143,7 +143,34 @@ function likes(names) {
     }
 }
 
+function deleteNth(arr, n) {
+    
+    //if (n < 2) return "error";
 
+    let objArr = {};
+    let nArr = [];
+
+    for (const item of arr) {
+        
+        if (!(item in objArr))objArr[item] = 0;
+        console.log(objArr);
+        
+
+        if (objArr[item] > n - 1) continue;
+        console.log(objArr);
+        nArr.push(item);
+        objArr[item] ++;
+        console.log(objArr);
+
+    }
+    return nArr;
+}
+
+console.log(deleteNth([1,1,3,3,7,2,2,2,2], 3));//, [1, 1, 3, 3, 7, 2, 2, 2])
+
+console.log(deleteNth([1, 1, 1, 1], 2)); // return [1,1]
+
+console.log(deleteNth([20, 37, 20, 21], 1)); // return [20,37,21]
 
 /* Не правильная функция лохопедро!!!
 function deleteNth(arr, n) {
