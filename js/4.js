dec300c42a068e1fc7a04f43e099c580ac5f41b7
Alex <50113949+Dudarik@users.noreply.github.com>
2020-02-26 08:45:33 +0500

    Задачка изи

--- js/4.js
+++ js/4.js
@@ -14,4 +14,28 @@ function add (x, y) {
   return add(sum, z)
 }
 //function add (x, y) { return y === 0 ? x : add(x ^ y, (x & y) << 1); }
-console.log(add(2,3))
+//console.log(add(2,3))
+
+function firstNonConsecutive (arr) {
+  if (arr.length == 0) return null
+
+  let start = arr[0]
+
+  for (let i = 0; i < arr.length; i++ ) {
+    console.log(start, +arr[i]);
+    
+    if (start !== arr[i]) return arr[i]
+
+    start++
+  }
+  return null
+
+  //firstNonConsecutive = arr => arr.length == 1 ? null : arr[0] + 1 != arr[1] ? arr[1] : firstNonConsecutive(arr.slice(1)) 
+ /*function firstNonConsecutive (arr) {
+  return (r=arr.find((x,i)=>i>0&&x!=arr[i-1]+1))===undefined?null:r
+}*/
+}
+
+console.log( firstNonConsecutive([ -8, -7, -6, -5, -4, -3, -2, -1 ]) );
+
+
