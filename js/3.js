4e2a1551377b337c8ea1606c54d90d09d64e4665
Alex <50113949+Dudarik@users.noreply.github.com>
2020-02-26 08:26:49 +0500

    changes

--- js/3.js
+++ js/3.js
@@ -93,9 +93,6 @@ function solution(nums){
 */
 //console.log(Math.abs(-2 - -3));
 console.log(solution([-6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20]));
-<<<<<<< HEAD
-// returns "-6,-3-1,3-5,7-11,14,15,17-20"
-=======
 // returns "-6,-3-1,3-5,7-11,14,15,17-20"
 
 
@@ -267,4 +264,3 @@ sumArr(ls)
 //console.log();
 
 //console.log(sumArr([50, 55, 56, 57, 58]));
->>>>>>> ffd62bb66ca8cb1eb4dfa8d007a933cc741bc475
